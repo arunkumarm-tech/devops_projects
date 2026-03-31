570e1a80f9868c39afb04f01207efdab07a9a2fa
arunkumarm-tech <arunmxbox@gmail.com>
2026-03-31 16:20:34 -0600

    Phase 5 Mastery: Multi-container networking and Persistent Redis Volumes

---
+++ phase5_compose/app.py
@@ -0,0 +1,26 @@
+import time
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis-db', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return f"<h1>Devops Master Arun!</h1><p>This multi-container app has been viewed {count} times.</p>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
